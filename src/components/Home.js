0f6cf698bbcc5179b8f1fee64b9e1c07cd3b8701
Balu Devulapalli <baludevulapalli@balus-mac-mini.lan>
2021-02-13 12:59:20 -0800

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
             className="absolute object-cover w-full h-full" 
             />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Ola, This is Lakshmi Narasimha AKA Bala!</h1>
+                <h1 className="text-6xl text-black-100 font-bold cursive leading-none lg:leading-snug home-name">Ola, This is Lakshmi Narasimha AKA Bala!</h1>
             </section>
         </main>
     );
